c22e1886d86886e104b5e7b0e382222e8e543103
refaelok <refaeloknin@gmail.com>
2020-06-09 00:02:20 +0300

    Trigger Cinemachine Cut Scene
    Trigger Cinemachine Cut Scene

---
+++ Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        PlayableDirector playableDirector;
+        bool alreadyTriggered = false;
+
+        private void Start()
+        {
+            playableDirector = GetComponent<PlayableDirector>();
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!alreadyTriggered && other.gameObject.tag == "Player")
+            {
+                playableDirector.Play();
+                alreadyTriggered = true;
+            }
+        }
+    }
+}
+
