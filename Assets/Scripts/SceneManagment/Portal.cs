f87ef61abf02cea2528db1f8b4d923a9bfebee9d
refaelok <refaeloknin@gmail.com>
2020-06-11 00:20:58 +0300

    Wait For Scene To Load
    Wait For Scene To Load

---
+++ Assets/Scripts/SceneManagment/Portal.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagment
+{
+    public class Portal : MonoBehaviour
+    {
+        [SerializeField] int SceneToLoad = -1;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Player")
+            {
+                StartCoroutine(Transition());
+            }
+        }
+
+        private IEnumerator Transition()
+        {
+            DontDestroyOnLoad(gameObject);
+            yield return SceneManager.LoadSceneAsync(SceneToLoad);
+            print("Scene Loaded");
+            Destroy(gameObject);
+        }
+    }
+}
+
