9859d4595d464ff5626161d23e37553aa47bc1e2
refaelok <refaeloknin@gmail.com>
2020-06-11 21:19:51 +0300

    Cross Scene References
    Cross Scene References

--- Assets/Scripts/SceneManagment/Portal.cs
+++ Assets/Scripts/SceneManagment/Portal.cs
@@ -1,14 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 namespace RPG.SceneManagment
 {
     public class Portal : MonoBehaviour
     {
+        enum DestinationIdentifier { A, B };
         [SerializeField] int SceneToLoad = -1;
+        [SerializeField] Transform spawwnPoint;
+        [SerializeField] DestinationIdentifier destination;
 
         private void OnTriggerEnter(Collider other)
         {
@@ -21,9 +26,33 @@ namespace RPG.SceneManagment
         private IEnumerator Transition()
         {
             DontDestroyOnLoad(gameObject);
+
             yield return SceneManager.LoadSceneAsync(SceneToLoad);
-            print("Scene Loaded");
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
             Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawwnPoint.transform.position);
+            player.transform.rotation = otherPortal.spawwnPoint.transform.rotation;
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal != this && portal.destination == destination)
+                {
+                    return portal;
+                }
+            }
+
+            return null;
         }
     }
 }
